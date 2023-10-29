d4c2e2e01afb8b88bf04658776ef76ce11bdadf3
Eugenia <st_shelkunova@list.ru>
2023-10-29 21:50:36 +0300

    src

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -1,9 +1,9 @@
 public class BonusMilesService {
 
-    public int calculate (int price) {
+    public int calculate(int price) {
         int result;
         int condition = 20; // количество рублей для одной бонусной мили
-        result = price/condition;
+        result = price / condition;
         return result;
     }
 }
